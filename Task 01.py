55d96f511e30684b8aab8b3beee689f1e5c2738d
Mikhail <mha.willy@gmail.com>
2022-12-20 21:21:00 +0700

    Task01

---
+++ Task 01.py
@@ -0,0 +1,12 @@
+number = int(input('Введите размер списка '))
+list = []
+sum = 0
+for i in range(number):
+    list_number = int(input(f'Введите число {i+1} '))
+    list.append(list_number)
+    if i % 2 != 0:
+        sum += list[i]
+
+
+print(list)
+print(f'Сумма нечетных чисел равна {sum}')
